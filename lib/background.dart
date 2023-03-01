f1cafd70259c82c99eaac04c1df34b73fdef9a79
TashiP1 <tashitakuley@gmail.com>
2023-03-01 20:12:27 +0600

    done with splash screen

---
+++ lib/background.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class custombackground extends StatelessWidget {
+  const custombackground({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        color: Color.fromARGB(225, 232, 104, 32),
+    ),);
+
+  }
+}
